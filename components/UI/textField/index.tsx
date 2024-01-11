cd23274b28076b5ae45bddda1b72d9eb7aa4d8cf
karanWD <@karanshamspirzadeh@gmail.com>
2024-01-11 22:59:15 +0330

    feat: develop create new  product information

--- components/UI/textField/index.tsx
+++ components/UI/textField/index.tsx
@@ -1,20 +1,20 @@
-import React, { FC } from "react";
-import { StyledTextField, StyledTextFieldContainer } from "@/components/UI/textField/styles";
-import { TextFieldProps, Typography } from "@mui/material";
+import React, { FC } from 'react'
+import { StyledTextField, StyledTextFieldContainer } from '@/components/UI/textField/styles'
+import { TextFieldProps, Typography } from '@mui/material'
 
 type InputProps = {
-  label: string,
-} & TextFieldProps;
+  label: string
+} & TextFieldProps
 
 const TextField: FC<InputProps> = ({ label, ...props }) => {
   return (
     <StyledTextFieldContainer>
-      <Typography variant={"body3"} className="label-container">
+      <Typography variant={'body3'} className="label-container">
         {label}
       </Typography>
       <StyledTextField {...props} />
     </StyledTextFieldContainer>
-  );
-};
+  )
+}
 
-export default TextField;
+export default TextField
