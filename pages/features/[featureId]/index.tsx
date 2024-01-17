87d804ceddfd48deb61595599756114ee96c7342
amiresmaeilzade <d.am.8.ir.w@gmail.com>
2024-01-17 08:56:14 +0330

    save pushes

---
+++ pages/features/[featureId]/index.tsx
@@ -0,0 +1,10 @@
+import {NextPage} from "next";
+import EditFeatureComponent from "@/components/features/editFeature";
+
+const EditFeaturePage: NextPage = () => {
+
+  return (
+    <EditFeatureComponent />
+  )
+}
+export default EditFeaturePage
